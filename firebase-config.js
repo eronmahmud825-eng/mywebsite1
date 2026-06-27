52b0c597cb9e21d25f610e91be047f1686e0ec4a
eronmahmud825-eng <eronmahmud825@gmail.com>
2026-06-27 23:01:41 +0300

    Refactor firebase-config.js for consistency

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,10 @@
-// firebase-config.js
-
 const firebaseConfig = {
   apiKey: "AIzaSyAcirrvSgPgb4zBkSMJg8G0WIqQdqqtOsM",
   authDomain: "consulate-d6691.firebaseapp.com",
   projectId: "consulate-d6691",
   storageBucket: "consulate-d6691.firebasestorage.app",
   messagingSenderId: "459011165720",
-  appId: "1:459011165720:web:40d50df68038219f49fd6d",
-  measurementId: "G-E5SQ8LW47N"
+  appId: "1:459011165720:web:40d50df68038219f49fd6d"
 };
-
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
